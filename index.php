e3d7cdb27c973ecb9d73337a4c39f4eb4991bacc
Oliverio Gombert <olivertar@gmail.com>
2018-11-07 09:45:29 -0300

    Without using any built in functions, only a for loop, return the next binary number in a string or as an array

--- index.php
+++ index.php
@@ -28,3 +28,68 @@ function reformat($string){
 ?>
 <h5><u>In:</u> <?php echo 'liMeSHArp DeveLoper TEST';?></h5>
 <h5><u>Out:</u> <?php echo reformat('liMeSHArp DeveLoper TEST');?></h5>
+
+<p>-------------------------------------------------------------------------------------</p>
+
+<h3>TEST 3</h3>
+<p><small>Without using any built in functions, only a for loop, return the next binary number in a string or as an array</small></p>
+
+<?php
+function next_binary_number($input, $type='array'){
+    $out = $outrev = array();
+    $outstring = '';
+    $carry = 0;
+    
+    $positions = count($input);
+    
+    for ($i = $positions; $i>0; $i--) {
+        if($i == $positions):
+            if($input[$i-1] == 1):
+                $carry = 1;
+                $outrev[] = 0;
+            else:
+                $outrev[] = 1;
+            endif;
+        else:
+            if($carry == 1 && $input[$i-1] == 0):
+                $outrev[] = 1;
+                $carry = 0;
+            elseif($carry == 1 && $input[$i-1] == 1):
+                $outrev[] = 0;
+            else:
+                $outrev[] = $input[$i-1];
+                $carry = 0;
+            endif;
+        endif;
+    }
+    
+    if($carry == 1):
+        $outrev[] = 1;
+    endif;
+
+    $pos = count($outrev);
+    for ($j = 0; $j < $pos; $j++) {
+        $out[$j] = $outrev[$pos-$j-1];
+        $outstring.= $outrev[$pos-$j-1];
+    }
+    
+    if($type == 'array'):
+        return $out;
+    else:
+        return $outstring;
+    endif;
+    
+}
+
+/* TEST CASES */
+
+$testinput = array(1,0);
+//$testinput = array(1,1);
+//$testinput = array(1,1,0);
+//$testinput = array(1,0,0,0,0,0,0,0,0,1);
+
+/* TEST CASES END */
+?>
+<h5><u>In:</u> <?php print_r($testinput);?></h5>
+<h5><u>Out:</u> <?php print_r(next_binary_number($testinput));?></h5>
+<h5><u>Out as String:</u> <?php print_r(next_binary_number($testinput, 'string'));?></h5>
